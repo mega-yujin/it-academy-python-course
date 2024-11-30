0039d2da6d3e5a8f7c6496303922b4abca51cb47
Evgeniy Syroezhkin <e.syroezhkin@cft.ru>
2024-11-30 13:49:14 +0300

    added task_1

--- src/homework.py
+++ src/homework.py
@@ -1,2 +1,8 @@
-print('Hello')
-print('Hi Alex!')
+is_admin = False
+
+is_blocked = True
+is_registered = True
+
+
+has_permission = True if is_registered and not is_blocked else False
+print(has_permission)
