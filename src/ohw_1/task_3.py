072d6b449d950a256156222f1434537acef0db64
Gleb Nadomin <gnadomin@gmail.com>
2024-12-04 17:41:43 +0300

    added task 3

---
+++ src/ohw_1/task_3.py
@@ -0,0 +1,15 @@
+month = int(input("Введите число: "))
+
+match month:
+    case month if month < 1:
+        print("Значение слишком маленькое")
+    case 12 | 1 | 2:
+        print("Зима")
+    case 3 | 4 | 5:
+        print("Весна")
+    case 6 | 7 | 8:
+        print("Лето")
+    case 9 | 10 | 11:
+        print("Осень")
+    case _:
+        print("Значение слишком большое")
