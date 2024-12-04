1cf0e1be8827963fdbf3884d6092173b7cbb1ec4
Lesha <aleksejmalusickij@gmail.com>
2024-12-04 23:29:12 +0300

    add task 3

---
+++ src/OHW_1/task_3.py
@@ -0,0 +1,14 @@
+month = int(input("Введите число: "))
+if 0 < month <= 12:
+    if 1 <= month <= 3:
+        print('зима')
+    elif 4 <= month <= 6:
+        print('весна')
+    elif 7 <= month <= 9:
+        print('лето')
+    else:
+        print('осень')
+elif month < 1:
+    print("значение слишком маленькое")
+else:
+    print("значение слишком большое")
