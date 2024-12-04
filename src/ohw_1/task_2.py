8585f4298d0557b463e0707a21249e78ffc4d407
Gleb Nadomin <gnadomin@gmail.com>
2024-12-04 17:34:45 +0300

    added task 2

---
+++ src/ohw_1/task_2.py
@@ -0,0 +1,13 @@
+num = int(input("Введите число: "))
+
+match num:
+    case 1:
+        print("Зима")
+    case 2:
+        print("Весна")
+    case 3:
+        print("Лето")
+    case 4:
+        print("Осень")
+    case _:
+        print("Нет такой поры года")
