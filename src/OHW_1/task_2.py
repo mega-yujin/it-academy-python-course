6ef8ec709ee21739226402293b08dccf896fe5ed
Lesha <aleksejmalusickij@gmail.com>
2024-12-04 23:28:07 +0300

    add task 2

---
+++ src/OHW_1/task_2.py
@@ -0,0 +1,15 @@
+num = int(input("Введите число: "))
+if num == 1:
+    result = 'зима'
+    print(result)
+elif num == 2:
+    result = 'весна'
+    print(result)
+elif num == 3:
+    result = 'лето'
+    print(result)
+elif num == 4:
+    result = 'осень'
+    print(result)
+else:
+    print("Нет такой поры года")
