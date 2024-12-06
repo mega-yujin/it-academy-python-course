cb63ec1e904501084fc6cdd2db95a9e61c36e8f4
hiruga-ruga <103525143+hiruga-ruga@users.noreply.github.com>
2024-12-06 20:43:18 +0300

    correction

--- src/ohw_1/task_2.py
+++ src/ohw_1/task_2.py
@@ -1,15 +1,13 @@
 num = int(input('Введите число:'))
 if num == 1:
     result = 'Зима'
-    print(result)
 elif num == 2:
     result = 'Весна'
-    print(result)
 elif num == 3:
     result = 'Лето'
-    print(result)
 elif num == 4:
     result = 'Осень'
-    print(result)
 else:
     print('Нет такой поры года')
+    exit()
+print(result)
--- src/ohw_1/task_4.py
+++ src/ohw_1/task_4.py
@@ -1,8 +1,8 @@
-is_admin = False
+is_admin = True
 is_active = True
-has_permission = False
+has_permission = True
 is_blocked = True
 
-is_allowed = is_blocked and (is_admin or (is_active and has_permission))
+is_allowed = not is_blocked and (is_admin or (is_active and has_permission))
 
 print(is_allowed)
