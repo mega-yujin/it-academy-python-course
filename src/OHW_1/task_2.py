b97e8d8d33a39cf1a8500eb709da2273d4bbb98a
Lesha <aleksejmalusickij@gmail.com>
2024-12-07 13:57:39 +0300

    made adjustments according to the comments in test_2 and test_4

--- src/OHW_1/task_2.py
+++ src/OHW_1/task_2.py
@@ -1,15 +1,12 @@
 num = int(input('Введите число: '))
 if num == 1:
     result = 'зима'
-    print(result)
 elif num == 2:
     result = 'весна'
-    print(result)
 elif num == 3:
     result = 'лето'
-    print(result)
 elif num == 4:
     result = 'осень'
-    print(result)
 else:
-    print('Нет такой поры года')
+    result = 'Нет такой поры года'
+print(result)
--- src/OHW_1/task_4.py
+++ src/OHW_1/task_4.py
@@ -2,5 +2,5 @@ is_admin = False
 is_active = True
 has_permission = True
 is_blocked = False
-is_allowed = True if not is_blocked and (is_admin or (is_active and has_permission)) else False
+is_allowed = not is_blocked and (is_admin or (is_active and has_permission))
 print(is_allowed)
