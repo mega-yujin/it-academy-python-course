8cf65bb515a061e28dd6c658a152430062395869
Gleb Nadomin <gnadomin@gmail.com>
2024-12-11 23:00:08 +0300

    added task 7

---
+++ src/mhw_2/task7.py
@@ -0,0 +1,36 @@
+"""Даны: три стороны треугольника.
+Требуется: проверить, действительно ли это стороны треугольника.
+Если стороны определяют треугольник, найти его площадь.
+Если нет, вывести сообщение о неверных данных.
+"""
+
+import math
+
+
+def triangle_inequality_theorem(sides: tuple[int, int, int]) -> bool:
+    return (
+        sides[0] + sides[1] > sides[2] and
+        sides[0] + sides[2] > sides[1] and
+        sides[1] + sides[2] > sides[0]
+    )
+
+
+def area_of_triangle_sides(sides: tuple[int, int, int]) -> float:
+    semi_perimeter = (sides[0] + sides[1] + sides[2])/2
+    return math.sqrt(
+        semi_perimeter * (semi_perimeter - sides[0]) *
+        (semi_perimeter - sides[1]) *
+        (semi_perimeter - sides[2])
+    )
+
+
+triangle_sides = (
+    int(input('Введите первую сторону: ')),
+    int(input('Введите вторую сторону: ')),
+    int(input('Введите третью сторону: ')),
+)
+
+if triangle_inequality_theorem(triangle_sides):
+    print(area_of_triangle_sides(triangle_sides))
+else:
+    print('Wrong sides')
