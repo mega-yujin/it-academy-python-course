e5cd7b105fb06c287ddf1bb1ae2c24fd0165338e
Qvantum70 <tima3907909@gmail.com>
2024-12-12 20:28:22 +0300

    Delete src/Task 4.py

--- src/Task 4.py
+++
@@ -1,8 +0,0 @@
-is_admin: bool = True
-is_active: bool = True
-is_blocked: bool = True
-has_permission: bool = False
-has_access: bool = (not is_blocked) and (is_admin or (is_active and has_permission))
-
-message = "доступ разрешён" if has_access else "доступ отклонён"
-print(message)
