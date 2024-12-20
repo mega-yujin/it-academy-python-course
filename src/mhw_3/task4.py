60130455cf556cede5f650e4d729a6747cef4b4f
Evgenii Delov <evgeniiy.delov@gmail.com>
2024-12-20 23:37:11 +0300

    Added homework for checking

---
+++ src/mhw_3/task4.py
@@ -0,0 +1,15 @@
+"""
+    Дан список чисел. Посчитайте, сколько в нем пар элементов, равных друг другу.
+    Считается, что любые два элемента, равные друг другу образуют одну пару, которую необходимо посчитать.
+    Входные данные - строка из чисел, разделенная пробелами.
+    Выходные данные - количество пар.
+    Важно: 1 1 1 - это 3 пары, 1 1 1 1 - это 6 пар
+"""
+
+entered_numbers = [int(s) for s in input().split()]
+pairs_number = 0
+for number in range(len(entered_numbers)):
+    for sum_number in range(number + 1, len(entered_numbers)):
+        if entered_numbers[number] == entered_numbers[sum_number]:
+            pairs_number += 1
+print(pairs_number)
