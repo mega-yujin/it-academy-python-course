9c9c597923b626ed503eb291d7df61cd0ce17a95
Evgenii Delov <evgeniiy.delov@gmail.com>
2024-12-24 21:01:16 +0300

    corrected the codestyle

--- src/task_3.py
+++ src/task_3.py
@@ -1,13 +1,13 @@
 month = int(input('Введите число: '))
-if month < 1:
+if month < 1:  # noqa: WPS223
     print('значение слишком маленькое')
-elif 1 <= month <= 2 or month == 12:
+elif 1 <= month <= 2 or month == 12:  # noqa: WPS223
     print('Зима')
-elif 3 <= month <= 5:
+elif 3 <= month <= 5:  # noqa: WPS223
     print('Весна')
-elif 6 <= month <= 8:
+elif 6 <= month <= 8:  # noqa: WPS223
     print('Лето')
-elif 9 <= month <= 11:
+elif 9 <= month <= 11:  # noqa: WPS223
     print('Осень')
 else:
     print('значение слишком большое')
