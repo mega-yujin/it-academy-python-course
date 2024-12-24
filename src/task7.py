64a52dace484b9e5257124a1f85bb47a680c06e2
Evgenii Delov <evgeniiy.delov@gmail.com>
2024-12-24 19:31:48 +0300

    corrected the codestyle

--- src/task7.py
+++ src/task7.py
@@ -6,13 +6,17 @@
 """
 
 print('Введите стороны треугольника: ')
-a = int(input('a = '))
-b = int(input('b = '))
-c = int(input('c = '))
-if a + b > c and a + c > b and b + c > a:
-    semi_perimeter = (a + b + c) / 2
-    s = semi_perimeter
-    triangle_area = (s * (s - a) * (s - b) * (s - c)) ** 0.5
+first_side = int(input('a = '))
+second_side = int(input('b = '))
+third_side = int(input('c = '))
+if (first_side + second_side > third_side
+        and first_side + third_side > second_side
+        and second_side + third_side > first_side):
+    semi_perimeter = (first_side + second_side + third_side) / 2
+    triangle_area = ((
+                    semi_perimeter * (semi_perimeter - first_side) *
+                    (semi_perimeter - second_side) *
+                    (semi_perimeter - third_side)) ** 0.5)
     triangle_area = round(triangle_area, 2)
     print(f'Площадь треугольника равна: "{triangle_area}"')
 else:
